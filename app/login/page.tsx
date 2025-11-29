6371de138f1d0cec432fd282e4284261020fc101
Franck KIKONDA N'ZE <kikondanze@gmail.com>
2025-11-29 09:27:24 +0100

    ajout de la page login

---
+++ app/login/page.tsx
@@ -0,0 +1,21 @@
+import { GalleryVerticalEnd } from "lucide-react"
+import Image from "next/image";
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
+    return (
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 px-6 md:p-0">
+            <div className="flex w-full max-w-sm flex-col gap-6">
+                <a href="#" className="flex items-center gap-2 self-center font-medium">
+                    <Image
+                        src="/images/logo1.png"
+                        alt="Logo"
+                        width={150}
+                        height={150}
+                    />
+                </a>
+                <LoginForm />
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h2>Ewuang WEB</h2>
+    <div className="flex justify-between py-2 px-10">
+      <h2>Ewuang WEB</h2>
+      <Link href="/login" className="text-body">Se connecter</Link>
+    </div>
   );
 }
