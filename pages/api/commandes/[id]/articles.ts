f8a9b7c04de728775a952e7293425b7f5a2a753d
Franck KIKONDA N'ZE <kikondanze@gmail.com>
2026-06-08 11:38:25 +0100

    hotfix

--- pages/api/commandes/[id]/articles.ts
+++ pages/api/commandes/[id]/articles.ts
@@ -108,7 +108,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             id,
             couleur,
             taille,
-            prix,
             stock,
             image
         )
